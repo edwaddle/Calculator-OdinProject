c0654055b4836223f30c82bfe4961fc14f1c4752
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-08-14 22:08:05 -0700

    Adding function works

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello chat");
+const add = document.querySelector(".add");
+const subtract = document.querySelector(".subtract");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+const equal = document.querySelector(".equal");
+
+const number = document.querySelectorAll(".number");
+
+const display = document.querySelector(".display");
+number.forEach((element) => {
+    element.onclick = () =>{
+        display.textContent += element.value;
+    }
+})
+
+const operator = document.querySelectorAll(".operator");
+add.onclick = () =>{
+    let firstNum = display.textContent;
+    display.textContent="";
+    equal.onclick = () => {display.textContent = parseFloat(display.textContent) + parseFloat(firstNum);}
+}
+
+
